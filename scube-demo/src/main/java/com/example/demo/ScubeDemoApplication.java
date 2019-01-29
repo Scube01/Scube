e1c3b4bbd10d27b560beba290d11ab60cc857972
Scube01 <46636263+Scube01@users.noreply.github.com>
2019-01-29 10:24:27 -0500

    Update ScubeDemoApplication.java

--- scube-demo/src/main/java/com/example/demo/ScubeDemoApplication.java
+++ scube-demo/src/main/java/com/example/demo/ScubeDemoApplication.java
@@ -1,13 +1,7 @@
-package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class ScubeDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScubeDemoApplication.class, args);
 		System.out.println("My first project!!");
 	}
 

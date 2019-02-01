2bc329d996e5574323fcb229e02dbac9c4613cf9
Scube01 <46636263+Scube01@users.noreply.github.com>
2019-02-01 09:40:34 -0500

    Update ScubeDemoApplication.java

--- scube-demo/src/main/java/com/example/demo/ScubeDemoApplication.java
+++ scube-demo/src/main/java/com/example/demo/ScubeDemoApplication.java
@@ -3,7 +3,7 @@ public class ScubeDemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("My first project!!");
-		Sysmtem.out.println("Testing WebHook");
+		System.out.println("Testing WebHook");
 	}
 
 }
